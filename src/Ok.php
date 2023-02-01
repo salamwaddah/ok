d589efa72d7bb09ae25cfad45c471f9f34c96f9b
Salam <salamwaddah@gmail.com>
2023-02-01 14:54:48 +0400

    feat: added ok functionality

---
+++ src/Ok.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace SalamWaddah\Ok;
+
+class Ok
+{
+    /**
+     * OK constant
+     */
+    public const OK = 'ok';
+
+    /**
+     * Returns ok as a string
+     *
+     * @return string
+     */
+    public function ok(): string
+    {
+        return self::OK;
+    }
+}
